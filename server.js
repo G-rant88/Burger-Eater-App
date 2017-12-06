8ab7a2738ef65d4fcc30a3ffdc11271497448a5b
unknown <benjah88@gmail.com>
2017-12-05 19:41:06 -0800

    almost done

--- server.js
+++ server.js
@@ -14,12 +14,51 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "Ben",
+  password: "bgrant88",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+});
+
 app.use(express.static('public'))
 
 app.get("/",function(req, res){
+	connection.query("SELECT * FROM burgers;", function(err, data) {
 
-	 res.render("index");
-})
+	 res.render("index", {burgers:data});
+});
+
+});
+
+app.post("/add", function(req, res){
+
+	connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burgeradd], function(err, data) {
+
+	res.redirect("/");
+});
+
+});
+
+app.post("/dev", function(req, res){
+	console.log(req.body);
+	// console.log(req.body.burger_name);
+
+	connection.query("UPDATE burgers SET devoured =? WHERE burger_name =?", [true, req.body.burger_name], function(err, data) {
+
+	res.redirect("/");
+});
+
+});
+
+
 
 app.listen(PORT, function() {
   console.log("burger app listening on port", PORT);
